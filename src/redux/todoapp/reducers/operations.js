710750f9352ac8393cf00f4070ab45b60f9a8444
Sai Siddarth <siddarthsiddu086@gmail.com>
2024-04-16 00:50:38 +0530

    created  addTodo and deleteAll reducer  functions

--- src/redux/todoapp/reducers/operations.js
+++ src/redux/todoapp/reducers/operations.js
@@ -1,15 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-  { id: 1, todo: "Buy Laptop", completed: false },
-  { id: 2, todo: "Master Redux", completed: false },
-  { id: 3, todo: "Watering Plants", completed: true },
-];
 
 export const operationsSlice = createSlice({
   name: "operations",
-  initialState,
-  reducers: {},
+  initialState : {
+    items: [ 
+      { id: 1, todo: "Buy Laptop", completed: false },
+      { id: 2, todo: "Master Redux", completed: false },
+      { id: 3, todo: "Watering Plants", completed: true },
+    ]
+  },
+  reducers: {
+    addTodo: (state, action) => {
+      state.items.push(action.payload)
+    },
+    deleteAll: (state,action) => {
+      return {items: []}
+
+    }
+    
+  },
 });
+export const {addTodo, deleteAll} = operationsSlice.actions;
 
 export default operationsSlice.reducer;
